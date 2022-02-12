9d30184ce6534e29c20bd2d5e09d51c1c3ed2faf
Родион Щербаков <sh20rodion02@gmail.com>
2022-02-12 16:28:06 +0600

    name

--- LAB/Models/Position.cs
+++ LAB/Models/Position.cs
@@ -8,7 +8,7 @@ namespace LAB.Models
     public class Position
     {
         public int Id { get; set; }
-        public string Positions { get; set; }
+        public string Name { get; set; }
        
         
     }
