2d0db640225bafca54fa24d771d04b7ed3a72ed4
Дмитрий Кеменов <mrpokemontype66@gmail.com>
2022-02-16 18:51:57 +0600

    абоба

--- LAB/Controllers/PurchaseRawsController.cs
+++ LAB/Controllers/PurchaseRawsController.cs
@@ -19,9 +19,15 @@ namespace LAB.Controllers
         }
 
         // GET: PurchaseRaws
+        private async Task<List<PurchaseRaw>> GetAllPurchase()
+        {
+            var purchase = await _context.PurchaseRaws.Include(u => u.Raw).ToListAsync();
+            return purchase;
+        }
         public async Task<IActionResult> Index()
         {
-            return View(await _context.PurchaseRaws.ToListAsync());
+            var purchase = await GetAllPurchase();
+            return View(purchase);
         }
 
         // GET: PurchaseRaws/Details/5
@@ -45,6 +51,10 @@ namespace LAB.Controllers
         // GET: PurchaseRaws/Create
         public IActionResult Create()
         {
+            SelectList raws = new SelectList(_context.Raws, "Id", "NameOfRaw");
+            SelectList employees = new SelectList(_context.Employees, "Id", "Surname");
+            ViewBag.RawsAll = raws;
+            ViewBag.Emp = employees;
             return View();
         }
 
@@ -53,15 +63,30 @@ namespace LAB.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Quantity,Sum,Date")] PurchaseRaw purchaseRaw)
+        public async Task<IActionResult> Create(PurchaseRaw purchaseRaw)
         {
+            var raw = _context.Raws.Where(u => u.Id == purchaseRaw.RawId).FirstOrDefault();
+            var budget = _context.Budgets.Where(u => u.Id == 1).FirstOrDefault(); 
             if (ModelState.IsValid)
             {
-                _context.Add(purchaseRaw);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(purchaseRaw);
+                if (purchaseRaw.Sum > budget.CountOfBudget)
+                {
+                    //вот тут короче каким то макаром нужно вывести ошибку что нет денег, сделай родной
+                }
+                else
+                {
+                    _context.Add(purchaseRaw);
+                    await _context.SaveChangesAsync();
+
+                    budget.CountOfBudget = budget.CountOfBudget - purchaseRaw.Sum;                   
+                    raw.Quantity += purchaseRaw.Quantity;
+                    raw.Sum += purchaseRaw.Sum;
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(Index));
+                }
+                }
+                return View(purchaseRaw);
         }
 
         // GET: PurchaseRaws/Edit/5
